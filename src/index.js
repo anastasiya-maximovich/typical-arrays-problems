41d60d7ba3cf67e2a44c852a0fcddb9bb54f2c19
anastasiya-maximovich <anastasiya025@gmail.com>
2020-10-01 17:45:40 +0300

    feat: correct 2 & 3 parts

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ return min = Math.min.apply(null, array);
 },
 
 exports.max = function max (array) {
-    if(!Array.isArray(array) || array === null || array[0] === null){
+    if(!Array.isArray(array) || array.length === undefined || array[0] === undefined){
         return 0; 
     }
     return max = Math.max.apply(null, array);
   },
 
 exports.avg = function avg (array) {
-    if(!Array.isArray(array) || array === null || array[0] === null){
+    if(!Array.isArray(array) || array.length === undefined || array[0] === undefined){
         return 0; 
     }
     return array.reduce((a, b) => (a + b)) / array.length;
