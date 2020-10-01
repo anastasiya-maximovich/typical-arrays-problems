44bc710ed4ecaa1873321391565e64b7ed54555a
anastasiya-maximovich <anastasiya025@gmail.com>
2020-10-01 17:44:16 +0300

    feat: add test. Test them.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = function min (array) { 
+    if(!Array.isArray(array) || array.length === undefined || array[0] === undefined){
+        return 0; 
+    }
+return min = Math.min.apply(null, array);
+},
 
 exports.max = function max (array) {
-  return 0;
-}
+    if(!Array.isArray(array) || array === null || array[0] === null){
+        return 0; 
+    }
+    return max = Math.max.apply(null, array);
+  },
 
 exports.avg = function avg (array) {
-  return 0;
+    if(!Array.isArray(array) || array === null || array[0] === null){
+        return 0; 
+    }
+    return array.reduce((a, b) => (a + b)) / array.length;
 }
