9d8a7e0ceb0e4f6ca76dc06392c2b81ed8d12356
anastasiya-maximovich <anastasiya025@gmail.com>
2020-10-01 18:02:45 +0300

    refactor: add same commits in tast

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ There are a few typical problems while you working with arrays, and your goal is
 You should implement 3 functions: `max`, `min`, and `avg` for searching minimal value, maximal value, and an average value of a given array.
 
 For example:
-```js
+``js
 min(1, 2, 3, 4) // returns 1;
 max(1, 2, 3, 4) // returns 4;
 avg(1, 2, 3, 4) // returns 2.5;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 
 exports.min = function min (array) { 
     if(!Array.isArray(array) || array.length === undefined || array[0] === undefined){
-        return 0; 
+        return 0;  
     }
 return min = Math.min.apply(null, array);
 },
 
 exports.max = function max (array) {
     if(!Array.isArray(array) || array.length === undefined || array[0] === undefined){
-        return 0; 
+        return 0;  //  if (!array || !array.length) -- подсказка от бывалого ))
     }
     return max = Math.max.apply(null, array);
   },
@@ -19,3 +19,5 @@ exports.avg = function avg (array) {
     }
     return array.reduce((a, b) => (a + b)) / array.length;
 }
+ // Выдавал ошибку в проверке - просил уделить внимание, если пустой массив
+ // Сама я после || писала array === null, ребята в Дискорте подсказали уделить внимание ДЛИНЕ массива
